fa7a1a4c0a0a61f82a23242b1ce6c9f7975f3753
Ranuka Perera <random@sawrc.com>
2017-05-14 17:02:10 +0530

    * changed formatting for backwards compatibility. * Added service files for using it.

--- trim_files.py
+++ trim_files.py
@@ -14,9 +14,9 @@ def parse_path_arg():
     return os.path.abspath(args.folder_path), args.file_keep_count
 
 
-def main(dir, file_keep_count):
+def main(folder_path, file_keep_count):
     # Get a list of files in the dir
-    file_list = os.listdir(dir)
+    file_list = os.listdir(folder_path)
     # Sort files alphabetically
     file_list.sort()
     # Count the number of files that needs to be removed
@@ -25,15 +25,16 @@ def main(dir, file_keep_count):
     number_of_files_to_remove = 0 if number_of_files_to_remove < 0 else number_of_files_to_remove
     removed_file_count = 0
     # Remove files
+    print("Found {} files in {}".format(number_of_files, folder_path))
     for i in range(number_of_files_to_remove):
-        full_file_path = os.path.abspath(os.path.join(dir, file_list[i]))
+        full_file_path = os.path.abspath(os.path.join(folder_path, file_list[i]))
         try:
             os.remove(full_file_path)
             removed_file_count += 1
         except OSError:
-            print(f"Could not remove file {full_file_path}")
-    print(f"Removed {removed_file_count}/{number_of_files_to_remove} files.")
-    print(f"At most {file_keep_count} files should be left.")
+            print("Could not remove file {}".format(full_file_path))
+    print("Removed {}/{} files.".format(removed_file_count, number_of_files_to_remove))
+    print("At most {} files should be left.".format(file_keep_count))
 
 
 if __name__ == "__main__":
